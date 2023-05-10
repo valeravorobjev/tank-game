2c96837a2d92f39261bad08aeea402d0be99adc3
Vorobjev Valery <vorobjevvalera@gmail.com>
2023-05-10 09:30:27 +0300

    fix bugs
    fix: track pisition

--- units/tank/components/track/track_engine.gd
+++ units/tank/components/track/track_engine.gd
@@ -12,12 +12,11 @@ func tracking(ground: Node, position: Vector2, rotation: float):
 	tank_rotation = rotation
 	
 	if tank_prev_position == Vector2.ZERO:
-		
 		tank_prev_position = tank_position
 		var track = track_res.instantiate()
 		
 		track.rotate(tank_rotation)
-		track.position = tank_position + Vector2(0, -1).rotated(tank_rotation)
+		track.position = tank_position + Vector2(0, -10).rotated(tank_rotation)
 		
 		ground.add_child(track)
 		
